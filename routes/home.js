bb3a5e4a75c97fc1509235452af9631433b86af5
Raheem Carridice <carridiceraheem871@gmail.com>
2023-04-18 05:58:18 -0500

    stil fix

--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router()
 const controllerHome = require('../controllers/home')
 
-router.get('/', controllerHome.getIndex)
+router.get('/', controllerHome.getIndex())
 
 module.exports = router
