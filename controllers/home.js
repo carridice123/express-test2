1c28d6c43740baf04ec86a85820b23b51ddb5b5e
Raheem Carridice <carridiceraheem871@gmail.com>
2023-04-18 06:16:50 -0500

    shold fix

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,5 @@
-module.exports = {
-    getIndex: (req,res)=>{
-        res.render('index.ejs')
-    }
-
-    
+const getIndex = (req,res)=>{
+    res.render('index.ejs')
 }
+
+module.exports = getIndex
