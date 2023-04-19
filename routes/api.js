486dac1819d33cfba085c47cad83ff9e90ef661f
Raheem Carridice <carridiceraheem871@gmail.com>
2023-04-19 14:51:44 -0500

    fixing mvc

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const controllerApi = require('../controlles/api.js')
-router.use(controllerApi)
+
 router.get('/api/:rapperName', controllerApi.rapper)
 
 module.exports = router
--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,7 @@
 const express = require('express')
+const router = express.Router()
 const controllerHome = require('../controlles/home.js')
 router.use(controllerHome)
-
 router.get('/', controllerHome.indexHome)
 
 module.exports = router
