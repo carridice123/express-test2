86c6b3fb01cee4f2041ed3ac84af325904cf1fe6
Raheem Carridice <carridiceraheem871@gmail.com>
2023-04-19 14:32:08 -0500

    fixiing mvc

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,5 @@
-let controllerHome = require('../controlles/home.js')
 const express = require('express')
+const controllerHome = require('../controlles/home.js')
 const router = express.Router()
 
 
