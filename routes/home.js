366dad60f0d2f471fd6803b95c5c5dc094cc231e
Raheem Carridice <carridiceraheem871@gmail.com>
2023-04-19 15:16:11 -0500

    fixingg mvc errors

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const controllerHome = require('../controlles/home.js')
-router.get('/', controllerHome.indexHome)
+router.get('/', controllerHome)
 
 module.exports = router
