0b01ab15f0137250f1f36e387e9ac104f5fdebf4
Raheem Carridice <carridiceraheem871@gmail.com>
2023-04-18 20:15:23 -0500

    fixed age and location

--- public/js/main.js
+++ public/js/main.js
@@ -8,12 +8,15 @@ async function apiRequest(){
 
         console.log(data)
         let section = document.querySelector('section')
-        let h2 = document.createElement('h2').innerText = 'Birth Name: ' + data.birthName
+        let h2 = document.createElement('h2')
+        h2.innerText = 'Birth Name: ' + data.birthName
         section.appendChild(h2)
-        let h3 = document.createElement('h2').innerText = 'Age is: ' + data.age
+        let h3 = document.createElement('h2')
+        h3.innerText = 'Age is: ' + data.age
         section.appendChild(h3)
 
-        let h4 = document.createElement('h2').innerText = 'Birth Location: ' + data.birthLocation
+        let h4 = document.createElement('h2')
+        h4.innerText = 'Birth Location: ' + data.birthLocation
         section.appendChild(h4)
 
     }catch(error){
