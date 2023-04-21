0f31adf8add8ea10d914d5085e35506bd7488af8
Raheem Carridice <carridiceraheem871@gmail.com>
2023-04-21 11:25:28 -0500

    sfa

--- public/js/main.js
+++ public/js/main.js
@@ -7,17 +7,7 @@ async function apiRequest(){
         const data = await response.json()
 
         console.log(data)
-        let section = document.querySelector('section')
-        let h2 = document.createElement('h2')
-        h2.innerText = 'Birth Name: ' + data.birthName
-        section.appendChild(h2)
-        let h3 = document.createElement('h2')
-        h3.innerText = 'Age is: ' + data.age
-        section.appendChild(h3)
-
-        let h4 = document.createElement('h2')
-        h4.innerText = 'Birth Location: ' + data.birthLocation
-        section.appendChild(h4)
+     data.age
 
     }catch(error){
         console.log(error)
