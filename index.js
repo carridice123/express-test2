fe6019b8774311b84f5bb2a7087308906940f159
Raheem Carridice <carridiceraheem871@gmail.com>
2023-04-21 09:07:43 -0500

    removed cors

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-
-const cors = require('cors')
-app.use(cors)
 const homeRoutes = require('./routes/home.js')
 const apiRoutes = require('./routes/api.js')
 app.set('view engine', 'ejs')
