e0e031fc8e8a3682019d9a604639eecad279971b
Raheem Carridice <carridiceraheem871@gmail.com>
2023-04-22 12:58:56 -0500

    fixing fetch on client side rm cors and age and birth location

--- public/js/main.js
+++ public/js/main.js
@@ -3,12 +3,25 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     const rapperName = document.querySelector('input').value
     try{
-        const response = await fetch(`https://real-jade-kingfisher-boot.cyclic.app/api/${rapperName}`)
+        const response = await fetch(`/api/${rapperName}`)
         const data = await response.json()
 
         console.log(data)
-        document.querySelector('section').innerText = data.age
 
+        let section = document.querySelector('section')
+
+        let h2 = document.createElement('h2')
+        h2.innerText = 'Birth Name: ' + data.birthName
+        section.appendChild(h2)
+
+        let h3 = document.createElement('h2')
+        h3.innerText = 'Age is: ' + data.age
+        section.appendChild(h3)
+
+        let h4 = document.createElement('h2')
+        h4.innerText = 'Birth Location: ' + data.birthLocation
+        section.appendChild(h4)
+        
     }catch(error){
         console.log(error)
     }
