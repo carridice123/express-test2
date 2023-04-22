59784ea87254da79d27426f24b7a693b4b914725
Raheem Carridice <carridiceraheem871@gmail.com>
2023-04-22 08:00:08 -0500

    use api route first in index js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-const homeRoutes = require('./routes/home.js')
 const apiRoutes = require('./routes/api.js')
+const homeRoutes = require('./routes/home.js')
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 
 
-app.use('/', homeRoutes)
 app.use('/api/:rapperName', apiRoutes)
+app.use('/', homeRoutes)
 app.listen(port, function(){
     console.log('listening on port 3000 go and catch its now');
 });
