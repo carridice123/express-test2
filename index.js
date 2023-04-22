0a3ee873256b4fbdc25673fd5a20d6bc1d230614
Raheem Carridice <carridiceraheem871@gmail.com>
2023-04-22 11:36:21 -0500

    fixing fetch on client side rm cors

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
 const port = process.env.PORT || 3000
 const homeRoutes = require('./routes/home.js')
 const apiRoutes = require('./routes/api.js')
@@ -10,8 +9,7 @@ app.use(express.static('public'))
 
 app.use('/', homeRoutes)
 
-app.use('api/:rapperName', apiRoutes)
-app.use(cors)
+app.use('/api/:rapperName', apiRoutes)
 app.listen(port, function(){
     console.log('listening on port 3000 go and catch its now');
 });
