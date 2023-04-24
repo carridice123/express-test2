719f75f41734c4ff3cd6391b24561344bbc19ab8
Raheem Carridice <carridiceraheem871@gmail.com>
2023-04-24 12:20:26 -0500

    simple rapper api with clear btn

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 document.querySelector('button').addEventListener('click', apiRequest)
-
+document.querySelector('.btn-1').addEventListener('click', removeRapper)
 async function apiRequest(){
     const rapperName = document.querySelector('input').value
     try{
@@ -9,6 +9,7 @@ async function apiRequest(){
         console.log(data)
 
         let section = document.querySelector('section')
+        
         let h2 = document.createElement('h2')
         h2.innerText = 'Birth Name: ' + data.birthName
         section.appendChild(h2)
@@ -20,11 +21,23 @@ async function apiRequest(){
         let h4 = document.createElement('h2')
         h4.innerText = 'Birth Location: ' + data.birthLocation
         section.appendChild(h4)
-        section.classList.toggle('remove-rapper')
+
     }catch(error){
         console.log(error)
     }
 }
+
+async function removeRapper(){
+    try{
+        const body = document.querySelector('body')
+        const section = document.querySelector('section')
+        body.removeChild(section)
+        location.reload()
+    }catch(error){
+        console.log(error)
+    }
+}
+
 console.log('hello world')
 
 
